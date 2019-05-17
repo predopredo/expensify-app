195dd0561d6a43d9d5aa22173f349677efda2b21
Pedro Santos <pedrosantosrochap@gmail.com>
2019-05-17 18:30:44 -0300

    adds production config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,58 @@
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // extracts css to a separate file
 
 const publicPath = path.join(__dirname, 'public');
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+  return {
     mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     entry: './src/app.js',
     output: {
-        path: publicPath,
-        filename: 'bundle.js'
+      path: publicPath,
+      filename: 'bundle.js'
     },
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
             {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
             },
             {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-
-            }]
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }]
     },
+    plugins: [
+      CSSExtract,
+    ],
     devServer: {
-        contentBase: publicPath,
-        port: 3000,
-        historyApiFallback: true // makes navigation work
+      contentBase: publicPath,
+      port: 3000,
+      historyApiFallback: true // makes navigation work
     }
+  };
 };
